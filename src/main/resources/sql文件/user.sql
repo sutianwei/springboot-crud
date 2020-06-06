0999c7e9c1cab43dec9a3dd70866ae880d7b64e3
苏天伟 <65763901+sutianwei@users.noreply.github.com>
2020-06-06 16:46:30 +0800

    sql文件

---
+++ src/main/resources/sql文件/user.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` varchar(15) DEFAULT NULL COMMENT '角色权限',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'admin', 'admin', 'admin');
